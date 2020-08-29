c10ba7f8736f4821849e48623af925d9097af907
nonusi2020 <nonu.si2020@gmail.com>
2020-08-29 13:24:29 +0530

    ticket:1 added html mockup and tests to resolve html

--- fortytwo_test_task/settings/common.py
+++ fortytwo_test_task/settings/common.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.fortytwotasks',
 ]
 
 MIDDLEWARE = [
@@ -77,7 +78,7 @@ WSGI_APPLICATION = 'fortytwo_test_task.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
