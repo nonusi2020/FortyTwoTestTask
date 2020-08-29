54e6cbdfd6c1c27ad086337dd5ce98aee5cfe875
nonusi2020 <nonu.si2020@gmail.com>
2020-08-29 17:57:14 +0530

    ticket:1 flake8 related changes added

--- apps/fortytwoapps/tests/test_views.py
+++ apps/fortytwoapps/tests/test_views.py
@@ -1,6 +1,5 @@
-from django.test import TestCase, RequestFactory
+from django.test import RequestFactory, TestCase
 from django.urls import reverse
-# Create your tests here.
 
 
 class ContactViewTestCase(TestCase):
--- apps/fortytwoapps/views.py
+++ apps/fortytwoapps/views.py
@@ -1,9 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from django.views.generic import TemplateView
-from django.template.response import TemplateResponse
 
-# Create your views here.
 
 class Contact(TemplateView):
     template_name = "fortytwoapps/contact.html"
