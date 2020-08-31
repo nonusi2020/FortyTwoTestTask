ab1abc7c8d5393a1dfa283c1c1baee94d8486c00
nonusi2020 <nonu.si2020@gmail.com>
2020-08-31 18:31:12 +0530

    ticket:1 added views and models for the contact and necessary changes in template.

--- apps/fortytwoapps/views.py
+++ apps/fortytwoapps/views.py
@@ -1,5 +1,11 @@
 from django.views.generic import TemplateView
+from apps.fortytwoapps.models import Contact
 
 
-class Contact(TemplateView):
+class ContactView(TemplateView):
     template_name = "fortytwoapps/contact.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(ContactView, self).get_context_data(**kwargs)
+        context['contact'] = Contact.objects.first()
+        return context
