720c44098f7bb9c8452b68a02c0eeb2c7878632d
nonusi2020 <nonu.si2020@gmail.com>
2020-09-01 18:42:45 +0530

    ticket:1 testing changes in urls.py

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -20,4 +20,5 @@ from apps.fortytwoapps.views import ContactView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('contact/', ContactView.as_view(), name='contact'),
+    path('', ContactView.as_view(), name='contact1'),
 ]
