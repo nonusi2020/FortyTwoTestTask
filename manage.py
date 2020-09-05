d5fce803eb31aaa80c88eee63ea4a2a30e27c1f4
nonusi2020 <nonu.si2020@gmail.com>
2020-09-05 17:01:44 +0530

    ticket:1 minor change

--- manage.py
+++ manage.py
@@ -14,7 +14,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    #added to execute loaddata
+
     if len(sys.argv) == 2 and sys.argv[1] == 'migrate':
         execute_from_command_line(['manage.py', 'loaddata','initial_data.json'])
 
